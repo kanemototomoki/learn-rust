276507db09a42285a8f8a53efe1878e4edbdccf0
kanemototomoki <kanemototomoki@gmail.com>
2022-12-08 11:17:08 +0900

    upd

--- leetcode/p_8/readme.md
+++ leetcode/p_8/readme.md
@@ -1,1 +1,4 @@
+解けてない
+低評価多い理由がわかったかも
+
 https://leetcode.com/problems/string-to-integer-atoi/
--- leetcode/p_8/src/main.rs
+++ leetcode/p_8/src/main.rs
@@ -1,4 +1,32 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let s = "words and 987".to_string();
+    // let s = "   -42-1241".to_string();
+    let mut result = "".to_string();
 
+    let mut count = 0;
+    for s in s.chars() {
+        println!("s: {:?}", s);
+        match s {
+            '-' | '+' => {
+                if count == 0 {
+                    count += 1;
+                    result.push_str(&s.to_string());
+                    continue;
+                }
+
+                break;
+                // if s == '-' {
+                // }
+            }
+            i @ '0'..='9' => result.push_str(&i.to_string()),
+            other => {
+                if other.is_alphanumeric() {
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("result: {}", result);
+    println!("result: {}", result.parse::<i32>().unwrap_or(0));
+}
