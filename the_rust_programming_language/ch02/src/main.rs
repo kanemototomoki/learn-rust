b498696b7fe3ec8e20448b86a91b783436de25cf
kanemototomoki <kanemototomoki@gmail.com>
2022-12-08 22:39:12 +0900

    done ch2

--- the_rust_programming_language/ch02/src/main.rs
+++ the_rust_programming_language/ch02/src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::{
+    cmp::Ordering,
+    io::{self, Write},
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を当ててごらん!");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    loop {
+        print!("数を入力してね!: ");
+        io::stdout().flush().unwrap();
+        let mut guess = "".to_string();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み取りに失敗しました。");
+
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎ！"),
+            Ordering::Greater => println!("大きすぎ！"),
+            Ordering::Equal => {
+                println!("正解！");
+                break;
+            }
+        }
+    }
 }
