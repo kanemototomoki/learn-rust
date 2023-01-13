fcae2aee5c71428a8b96bd7cb3837b14e84f719c
kanemototomoki <kanemototomoki@gmail.com>
2023-01-13 10:22:38 +0900

    wip

--- rust-learn-webapp-book/section03/src/main.rs
+++ rust-learn-webapp-book/section03/src/main.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+mod repositories;
+
+use crate::handlers::{all_todo, create_todo, delete_todo, find_todo, update_todo};
+use crate::repositories::{TodoRepository, TodoRepositoryForMemory};
+use axum::{
+    extract::Extension,
+    routing::{get, post},
+    Router,
+};
+use std::net::SocketAddr;
+use std::{env, sync::Arc};
+
+#[tokio::main]
+async fn main() {
+    // initialize logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LIG", log_level);
+    tracing_subscriber::fmt::init();
+
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
+
+    // ip address binding
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    // serve
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app<T: TodoRepository>(repository: T) -> Router {
+    // set routing
+    Router::new()
+        .route("/", get(root))
+        .route("/todos", post(create_todo::<T>).get(all_todo::<T>))
+        .route(
+            "todos/:id",
+            get(find_todo::<T>)
+                .delete(delete_todo::<T>)
+                .patch(update_todo::<T>),
+        )
+        .layer(Extension(Arc::new(repository)))
+}
+
+async fn root() -> &'static str {
+    "Hello world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::repositories::{CreateTodo, Todo};
+    use axum::response::Response;
+    use axum::{
+        body::Body,
+        http::{header, Method, Request, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    fn build_todo_req_with_json(path: &str, method: Method, json_body: String) -> Request<Body> {
+        Request::builder()
+            .uri(path)
+            .method(method)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(json_body))
+            .unwrap()
+    }
+
+    fn build_todo_req_with_empty(path: &str, method: Method) -> Request<Body> {
+        Request::builder()
+            .uri(path)
+            .method(method)
+            .body(Body::empty())
+            .unwrap()
+    }
+
+    async fn res_to_todo(res: Response) -> Todo {
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let todo: Todo = serde_json::from_str(&body).expect(&format!("cannot convert Todo instance. body: {}", body));
+        todo
+    }
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let repository = TodoRepositoryForMemory::new();
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+
+        assert_eq!(body, "Hello world!");
+    }
 }
